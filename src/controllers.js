975d12a1f3a9c8f7685c74a79b3cb7fd80d71b6d
Albert Parrón Herrera <al.parron@gmail.com>
2017-01-09 10:27:10 +0100

    chore(test): Add reporting to codecov

--- src/controllers.js
+++ src/controllers.js
@@ -1,4 +1,4 @@
-var numbers = [0,1,2,3,4,5,6,7,8,9,10];
+var numbers = [0,1,2,3,4,5,6,7,8,9];
 var uniqueRandomArray = require('unique-random-array');
 
 exports.Random = (number) => {
--- src/controllers.test.js
+++ src/controllers.test.js
@@ -1,18 +1,18 @@
 var expect = require('chai').expect;
 var Controllers = require('./controllers');
 
+function isArrayOfNumbers(array) {
+    return array.every(function(item){
+        return typeof item === 'number';
+    });
+}
+
 describe('Controllers', function(){
     describe('All', function(){
         it('Should be an array of numbers', function(){
             expect(Controllers.All()).to.satisfy(isArrayOfNumbers);
-            
-            function isArrayOfNumbers(array) {
-                return array.every(function(item){
-                    return typeof item === 'number';
-                });
-            }
         })
-        it('Should contain 9', function(){
+        it('Should contain number 9', function(){
             expect(Controllers.All()).to.include(9);
         })
     })
@@ -27,6 +27,28 @@ describe('Controllers', function(){
             randomItems.forEach(function(item){
                 expect(Controllers.All()).to.include(item);
             })
+        })
+    })
+    describe('Odd', function(){
+        it('Should return an array of numbers', function(){
+            expect(Controllers.Odd()).to.satisfy(isArrayOfNumbers);
+        })
+        it('Should contain 5 numbers', function(){
+            expect(Controllers.Odd()).to.have.length(5);
+        })
+        it('Should contain number 5', function(){
+            expect(Controllers.Odd()).to.include(5)
+        })
+    })
+    describe('Even', function(){
+        it('Should return an array of numbers', function(){
+            expect(Controllers.Even()).to.satisfy(isArrayOfNumbers);
+        })
+        it('Should contain 5 numbers', function(){
+            expect(Controllers.Even()).to.have.length(5);
+        })
+        it('Should contain number 6', function(){
+            expect(Controllers.Even()).to.include(6)
         })
     })
 });
